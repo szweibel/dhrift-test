d4f9824bbd1624da282b9517ea7b853b9cb38c0c
Stephen Zweibel <oneperstephen@gmail.com>
2024-09-11 11:13:32 -0400

    Update Webvm.js

--- components/Wasm/Webvm.js
+++ components/Wasm/Webvm.js
@@ -6,7 +6,7 @@ export default function Webvm(props) {
     return (
         <div className='webvm'>
             <Head>
-                <script src='/coi-serviceworker.js' async></script>
+                <script src={process.env.NEXT_PUBLIC_ASSET_PREFIX +'/coi-serviceworker.js'} async></script>
             </Head>
             {/* <EditorTopbar {...props} /> */}
             <iframe src='../../webvm/index.html' width='100%' height='95%' id="iframe"></iframe>
